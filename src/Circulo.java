0eb223880c5de862c137aa62d0917b7f94faca00
jaimeperezrisco <jaime.perez00vsl@gmail.com>
2025-03-18 11:16:30 -0100

    add new methods

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,3 +1,10 @@
 public class Circulo extends Figura2D {
+
+    @Override
+    double calcularPerimetro() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calcularPerimetro'");
+    }
+    
     
 }
--- src/Figura2D.java
+++ src/Figura2D.java
@@ -1,3 +1,23 @@
-public class Figura2D extends Figura {
+abstract class Figura2D extends Figura {
+
+    //preguntar abajo
+    @Override
+    double calcularArea() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calcularArea'");
+    }
+
+    abstract double calcularPerimetro();
+
+    public void mostrarInformacion() {
+        //tipo de figura
+        System.out.println("El tipo de figura es + algo");
+        //area
+        System.out.println("El area es " + calcularArea());
+        //perimetro
+        System.out.println("El area es " + calcularPerimetro());
+
+    }
+
     
 }
